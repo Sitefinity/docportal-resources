3ada7d1d5a738a67819ab41d24e4e466d572ed1f
Galya Kandzhikova <gkandzhi@progress.com>
2024-06-06 10:43:27 +0300

    Add IIS1o Windows Server 2022

---
+++ Power Shell scripts/IIS10_Windows_Server_2022.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference  = "Stop"  
+     
+Import-Module ServerManager
+
+$features = @(
+    "Web-Server",
+    "Web-WebServer",
+    "Web-Mgmt-Console",
+    "Web-Static-Content",
+    "Web-Asp-Net45",
+    "Web-Net-Ext45",
+    "Web-Health",
+    "Web-Http-Logging",
+    "Web-Performance",
+    "Web-Stat-Compression"
+)
+
+Write-Host "Installing features..." -ForegroundColor Yellow
+
+Add-WindowsFeature $features
+
+Write-Host "Features installed." -ForegroundColor Green
